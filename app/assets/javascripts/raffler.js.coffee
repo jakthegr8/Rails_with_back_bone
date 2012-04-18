a368ba4531a620506960764ea726403abdd6aed0
Arun <jakheart001@gmail.com>
2012-04-18 00:07:25 -0400

    Adding basic structure for rails with back bone js

---
+++ app/assets/javascripts/raffler.js.coffee
@@ -0,0 +1,9 @@
+window.Raffler =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  init: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Raffler.init()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'ca7aff98f326087a2f2d7dca7a29956c6f1a845a2e287435945f7c9df787804a881920142753cd46fd4ea1193cd06405bcac40b202fce5d3179311df3017f29a'
